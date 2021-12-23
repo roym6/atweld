c10eaae6d8efda2bd6e3e9cc052149a084cbde86
Rodrigo Manzano <roymanzano6@gmail.com>
2021-12-23 14:20:01 -0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+npx tailwindcss -i ./src/input.css -o ./dist/output.css
+cp src/index.html dist/
+cp src/temp.jpg dist/
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,4 @@ if [ -z "$1" ]
 then
     folder=$1
 fi
-cp src/index.html dist/
-cp src/temp.jpg dist/
 git subtree push --prefix dist origin gh-pages
